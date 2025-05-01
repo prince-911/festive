4e90ef02bf7afd243014d62d685b4846bc21a095
Prince Kumar <145214459+prince-911@users.noreply.github.com>
2025-05-02 00:40:20 +0530

    Update tables.sql

--- festive_ride1/tables.sql
+++ festive_ride1/tables.sql
@@ -1,1 +1,7 @@
+create database festive_ride;
 
+use festive_ride;
+
+create table admin(username varchar(30) primary key,firstname varchar(20) not null,
+middlename varchar(20),lastname varchar(20),email varchar(40) not null,number varchar(20) not null
+,password varchar(40) not null);
