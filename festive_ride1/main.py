09fc8d0af4b2ed74b2891c9291aef7ce73df4450
Prince Kumar <145214459+prince-911@users.noreply.github.com>
2025-05-02 00:39:03 +0530

    Update main.py

--- festive_ride1/main.py
+++ festive_ride1/main.py
@@ -1,1 +1,13 @@
+from db import mysql
 
+def registration(username,firstname,middlename,lastname,email,number,password,confirm_password):
+    if(password==confirm_password):
+        conn=mysql.connect
+        cur=conn.cursor()
+        cur.execute("insert into admin (username,firstname,middlename,lastname,email,number,password) values (%s,%s,%s,%s,%s,%s,%s)",(username,firstname,middlename,lastname,email,number,password))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    else:
+        return False
